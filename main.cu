f15f2837d710c5821a0652c9dc51bca3e26d86c3
Austin <Austin.W.Snyder@outlook.com>
2025-05-28 21:29:14 -0700

    graph vis of the tsp dataset

--- main.cu
+++ main.cu
@@ -5,17 +5,34 @@
 #include <cuda_gl_interop.h>
 #include <thrust/device_ptr.h>
 #include <thrust/sort.h>
-#include <iostream>
 #include <vector>
+#include <limits>
 #include <cstdlib>
+#include <utility>
 #include <ctime>
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <sstream>
+using namespace std;
 
 #define USE_CPU false
+#define PI 3.14159265358979323846f
 
 cudaGraphicsResource* cudaBoidVBOResource;
 GLuint boidVBO;
 
 __host__ void createBoidVBO(GLuint* vbo, int numBoids);
+
+
+
+
+vector<pair<float, float>> importDataset(std::string fileName);
+void render(vector<vector<float>> adjacencyList, vector<glm::vec2> nodePositions);
+void drawCircle(glm::vec2 center, float r);
+vector<glm::vec2> computeNodePositions(int numNodes, vector<pair<float, float>> coords);
+vector<vector<float>> convertToGraph(int dimension, vector<pair<float, float>> coords);
+
 
 int CUDA(int argc, char** argv);
 
@@ -188,7 +205,7 @@ __host__ void createBoidVBO(GLuint* vbo, int numBoids) {
 int CUDA(int argc, char** argv) {
     if (!glfwInit()) return -1;
 
-    GLFWwindow* window = glfwCreateWindow(1000, 1000, "CUDA OpenGL Boids", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(800, 800, "CUDA OpenGL Boids", nullptr, nullptr);
     if (!window) return -1;
 
     glfwMakeContextCurrent(window);
@@ -291,5 +308,230 @@ int CUDA(int argc, char** argv) {
 
 int main(int argc, char** argv) {
     // Removed CPU version entirely
-    return CUDA(argc, argv);
+
+    vector<pair<float, float>> coordinates = importDataset("C:\\Users\\asnyd\\CLionProjects\\CudaOpenGLFlocking\\datasets\\dj38.tsp");
+    int N = coordinates.size();
+    vector<vector<float>> graph = convertToGraph(N, coordinates);
+    vector<glm::vec2> nodesVis = computeNodePositions(N, coordinates);
+
+    if (!glfwInit()) return -1;
+
+    GLFWwindow* window = glfwCreateWindow(1200, 1200, "Graph Visualizer", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glewInit();
+
+    // Setup 2D orthographic projection
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, 1, 0, 1, 0, 1);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    while (!glfwWindowShouldClose(window)) {
+        render(graph, nodesVis);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+
+    return 0;
+    //CUDA(argc, argv);
+}
+
+
+
+
+
+
+
+
+
+
+// Data Mule Routing Problem (with Limited Autonomy)
+
+/**
+ * Essentially the papers read aim to travel to wireless sensors with drones.
+ * These sensors have a set radius that they can communicate with the drone to transmit information.
+ * We need to leave from the "base station" and travel in a path that collects all the sensor information
+ * in the smallest amount of time.
+ *
+ *
+ * In terms of time, both training time to find the solution, and the optimality of our paths matter heavily.
+ * In wild-land rescue, a model that trains for 10 hours could cause someone to never be found or even die.
+ *
+ *
+ * Since we have sensors with a radius of communication, there are possible overlaps between sensor
+ *
+ *
+ * I cannot find the online datasets that they used because they are locked inside a springer publication
+ *
+ * Instead, i have some TSP datasets that I will use and add in the radius manually. Additionally, the first
+ * node in the set will be the "base"
+ */
+void createDMRP(int numSensors, float radius, int numDrones) {
+
+
+
+
+}
+
+
+// Remember to ask Dr. Davendra:
+// Ant Colony Optimization with TSP / TSP with neighborhoods
+// TSP common approaches in general.
+// Papers about common algorithms in Scientific Computing and their pros and cons.
+
+
+float euclideanDistance(const std::pair<float, float>& p1, const std::pair<float, float>& p2) {
+    float dist = 0.0f;
+
+    dist += pow(p1.first - p2.first, 2);
+    dist += pow(p1.second - p2.second, 2);
+
+    return sqrt(dist);
+}
+
+/**
+ * Imports a traveling salesman type dataset.
+ */
+vector<pair<float, float>> importDataset(std::string fileName) {
+    std::ifstream file(fileName);
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file.\n";
+        exit(1);
+    }
+
+    std::string line;
+    std::vector<std::pair<float, float>> coordinates;
+
+    bool readCoord = false;
+    int dimension = 0;
+
+    while(std::getline(file, line)) {
+        // We have the dimension now.
+        if(line.find("DIMENSION") != std::string::npos) {
+            dimension = std::stoi(line.substr(line.find(":") + 1));
+            coordinates.resize(dimension);
+        }
+
+        // We have reached the coordinate section of the dataset
+        if(line.find("NODE_COORD_SECTION") != std::string::npos) {
+            readCoord = true;
+            continue;
+        }
+
+        // Add in the line that is like id x y into the coords.
+        if(readCoord) {
+            // Take the line, it will be split 3 times: id, x, y
+            std::istringstream iss(line);
+            int id;
+            double x, y;
+            if(!(iss >> id >> x >> y)) break;
+            coordinates[id - 1] = std::make_pair(x, y);
+        }
+    }
+
+    return coordinates;
+}
+
+vector<vector<float>> convertToGraph(int dimension, vector<pair<float, float>> coordinates) {
+    // Now we want to actually make the graph representation now, in adjacency list.
+    std::vector<std::vector<float>> dist(dimension, std::vector<float>(dimension));
+    for(int i = 0; i < dimension; i++) {
+        for(int j = 0; j < dimension; j++) {
+            dist[i][j] = euclideanDistance(coordinates[i], coordinates[j]);
+        }
+    }
+
+    // Lets see if it was made correctly.
+    for(const auto& coord : dist) {
+        for(int i = 0; i < dimension; i++) {
+            std::cout << coord[i] << ", ";
+        }
+        std::cout << std::endl;
+    }
+
+    return dist;
+}
+
+
+/**
+ * Convert the actual longitude and latitudes into a version we can render.
+ * @param N
+ * @param coordinates
+ * @return
+ */
+vector<glm::vec2> computeNodePositions(int N, vector<pair<float, float>> coordinates) {
+    vector<glm::vec2> nodePositions(0);
+    float minX = numeric_limits<float>::infinity();
+    float maxX = -numeric_limits<float>::infinity();
+
+    float minY = numeric_limits<float>::infinity();
+    float maxY = -numeric_limits<float>::infinity();
+
+    // Get mins and maxes of the "space", i want to plot how it actually looks.
+    for(int i = 0; i < N; i++) {
+        maxX = std::max(maxX, coordinates[i].first);
+        minX = std::min(minX, coordinates[i].first);
+
+        maxY = std::max(maxY, coordinates[i].second);
+        minY = std::min(minY, coordinates[i].second);
+    }
+
+    // Some buffer on the border
+    maxX += 50;
+    maxY += 50;
+    minX -= 50;
+    minY -= 50;
+
+    // Normalize the coordinates to a 0-1 range to allow visualization
+    for (int i = 0; i < N; ++i) {
+        float x = (coordinates[i].first - minX) / (maxX - minX);
+        float y = (coordinates[i].second - minY) / (maxY - minY);
+        nodePositions.emplace_back(x, y);
+    }
+
+    return nodePositions;
+}
+
+void drawCircle(glm::vec2 center, float r) {
+    glBegin(GL_TRIANGLE_FAN);
+    glVertex2f(center.x, center.y);
+
+    for (int i = 0; i <= 20; ++i) {
+        float theta = 2.0f * PI * i / 20;
+        glVertex2f(center.x + r * cos(theta), center.y + r * sin(theta));
+    }
+
+    glEnd();
+}
+
+void render(vector<vector<float>> adjacencyList, vector<glm::vec2> nodePositions) {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glColor3f(1.0f, 1.0f, 1.0f); // white lines
+    glBegin(GL_LINES);
+    for (int i = 0; i < adjacencyList.size(); ++i) {
+        for (int j = 0; j < adjacencyList[i].size(); j++) {
+
+            glm::vec2 a = nodePositions[i];
+            glm::vec2 b = nodePositions[j];
+            glVertex2f(a.x, a.y);
+            glVertex2f(b.x, b.y);
+
+        }
+    }
+    glEnd();
+
+    glColor3f(0.2f, 0.6f, 1.0f); // blue nodes
+    for (const auto& pos : nodePositions) {
+        drawCircle(pos, 0.01f);
+    }
 }
